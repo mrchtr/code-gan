45f2f22bd568c53ecce56ce9ef5972fbf401f869
Matthias Richter <matthias.richter@doksafe.de>
2021-10-19 08:24:50 +0200

    - update config.py

--- config.py
+++ config.py
@@ -79,9 +79,9 @@ def init_config():
     size of the given context for the sequence generation
     """
     config.start_sequence_len = 20 #25
-    config.batch_size = 32
+    config.batch_size = 64
     # config.pretraining_epochs = 0
-    config.pretraining_steps = 1000
+    config.pretraining_steps = 5000
     config.lr_pretrain = 5e-5
     config.lr_adv_g = 1e-4  # 1e-4
     config.lr_adv_d = 1e-4  # 1e-4
