bb03c97d4b2e38b5d44259f80544f32f5e6ed327
mrchtr <matthias.r1092@gmail.com>
2021-10-19 15:07:05 +0200

    - update config.py

--- config.py
+++ config.py
@@ -81,11 +81,11 @@ def init_config():
     config.start_sequence_len = 20 #25
     config.batch_size = 64
     # config.pretraining_epochs = 0
-    config.pretraining_steps = 1000
+    config.pretraining_steps = 10000
     config.lr_pretrain = 5e-5
     config.lr_adv_g = 1e-4  # 1e-4
     config.lr_adv_d = 1e-4  # 1e-4
-    config.nadv_steps = 100000
+    config.nadv_steps = 50000
     config.g_steps = 1
     config.d_steps = 1
     config.temperature = 50
