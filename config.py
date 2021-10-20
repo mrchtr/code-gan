f03318adf5145461c71c6ab420568c698fd47ffa
Matthias Richter <matthias.richter@doksafe.de>
2021-10-20 11:06:26 +0200

    - edit config.py

--- config.py
+++ config.py
@@ -81,12 +81,12 @@ def init_config():
     config.start_sequence_len = 20 #25
     config.batch_size = 64
     # config.pretraining_epochs = 0
-    config.pretraining_steps = 2000
+    config.pretraining_steps = 10000
     config.lr_pretrain = 5e-5
     config.lr_adv_g = 1e-4  # 1e-4
     config.lr_adv_d = 1e-4  # 1e-4
     config.nadv_steps = 50000
-    config.g_steps = 32
+    config.g_steps = 5
     config.d_steps = 1
     config.temperature = 50
     config.loss_type = "wgan-gp" #standard, rsgan, wgan or wgan-gp
