a986c8d7bb8a8cfde314689ca7f00a0272da6926
mrchtr <matthias.r1092@gmail.com>
2021-10-21 18:47:57 +0200

    - config

--- config.py
+++ config.py
@@ -70,7 +70,7 @@ def init_config():
     # Pretraining
     config.pretrain_optimizer = "AdamW"
     config.lr_pretrain = 5e-5
-    config.pretraining_epochs = 0
+    config.pretraining_epochs = 5
 
     # GAN training
     config.generator_optimizer = "Adam"
