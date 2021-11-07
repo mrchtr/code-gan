5427ab904f71fd848cdaaada4121c25a4b85683f
mrchtr <matthias.r1092@gmail.com>
2021-11-07 11:10:26 +0100

    - bert real config wgan-gp

--- config.py
+++ config.py
@@ -87,9 +87,9 @@ def init_config():
     config.g_steps = 1
     config.d_steps = 1  # proposed by relgan
     config.temperature = 100  # proposed by relgan
-    config.loss_type = "wgan"  # standard, rsgan, wgan or wgan-gp
+    config.loss_type = "wgan-gp"  # standard, rsgan, wgan or wgan-gp
     config.clip_norm = 2
-    config.freezing_discriminator = False  # freeze layers of pretrained discriminator ?
+    config.freezing_discriminator = True  # freeze layers of pretrained discriminator ?
     config.freezing_generator = False  # freeze layers of the pretrained generator ?
     config.repetition_penalty = 1.2
     config.sampling = "top_k"
