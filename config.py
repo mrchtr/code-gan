79a7feaeb385f1c7cc7331a9922c81ea5c1571fb
mrchtr <matthias.r1092@gmail.com>
2021-11-23 14:44:49 +0100

    - config change

--- config.py
+++ config.py
@@ -73,9 +73,9 @@ def init_config():
     config.batch_size = 48 # 64
 
     # Pretraining
-    config.pretrain_optimizer = "Adam"
-    config.lr_pretrain = 1e-3
-    config.pretraining_epochs = 10
+    config.pretrain_optimizer = "AdamW"
+    config.lr_pretrain = 5e-5
+    config.pretraining_epochs = 20
 
     # GAN training
     config.generator_optimizer = "Adam"
